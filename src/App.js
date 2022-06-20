5d493fe7946cdfc64090bf6f5b8ba9e01c08e256
Himanshu Srivastava <h.dataconsultancy@gmail.com>
2022-06-20 23:48:56 +0530

    Toggle Dark Mode

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import Alert from "./components/Alert";
 // import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
@@ -8,11 +9,24 @@ function App() {
 	const [mode, setMode] = useState("light");
 	// Whether dark mode is enabled or not
 
+	const [alert, setAlert] = useState(null);
+
+	const showAlert = (message, type) => {
+		setAlert({
+			msg: message,
+			type: type,
+		});
+	};
+
 	const toggleMode = () => {
 		if (mode === "light") {
 			setMode("dark");
+			document.body.style.backgroundColor = "#042743";
+			showAlert("Dark Mode has been enabled", "success");
 		} else {
 			setMode("light");
+			document.body.style.backgroundColor = "white";
+			showAlert("Light Mode has been enabled", "success");
 		}
 	};
 	return (
@@ -23,6 +37,7 @@ function App() {
 				mode={mode}
 				toggleMode={toggleMode}
 			/>
+			<Alert alert={alert} />
 			<TextForm heading="Enter text in the area below" />
 			{/* <About /> */}
 		</div>
